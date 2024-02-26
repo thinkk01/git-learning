0b9affac0a43eb052e0f246fd8dd9c86be37c12e
thinkk01 <thinhbirant1305@gmail.com>
2024-02-26 09:47:38 +0700

    First commit

---
+++ learning-git.cs
@@ -0,0 +1,10 @@
+namespace MyNamespace
+{
+    class Program{
+        static void public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello word!");
+                       
+        }
+    }
+}
